add0dbe6754920a3217b6cee68d61abf6dd536d8
brandonqlee <brandonqlee@hotmail.com>
2020-11-27 15:15:15 -0500

    cnn

--- model.py
+++ model.py
@@ -13,54 +13,104 @@ class YelpClassifier(tf.keras.Model):
         # self.linear = tf.keras.layers.Dense(512)
 
         self.batch_size = 100
-        self.num_epoch = 1
-        self.num_classes = 9    # ratings of 1.0, 1.5 ... 5.0 stars
-        self.dropout_rate = 0.5
-        self.learning_rate = 1e-3
-        self.hidden_layer1 = 80
-        self.hidden_layer2 = 40
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.learning_rate)
+        self.num_classes = 5
+        self.loss_list = [] # Append losses to this list in training so you can visualize loss vs time in main
 
-        self.conv1 = tf.Variable(tf.random.truncated_normal([5, 5, 3, 5], stddev=0.1))
-        self.b1 = tf.Variable(tf.random.truncated_normal([5], stddev=0.1))
+        self.learning_rate = 0.001
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
 
-        self.conv2 = tf.Variable(tf.random.truncated_normal([5, 5, 5, 10], stddev=0.1))
-        self.b2 = tf.Variable(tf.random.truncated_normal([10], stddev=0.1))
+        self.F1 = tf.Variable(tf.random.truncated_normal([5, 5, 3, 16], stddev=.1, dtype=tf.float32))
+        self.b1_F = tf.Variable(tf.random.truncated_normal([16], stddev=.1, dtype=tf.float32))
+        self.F2 = tf.Variable(tf.random.truncated_normal([5, 5, 16, 20], stddev=.1, dtype=tf.float32))
+        self.b2_F = tf.Variable(tf.random.truncated_normal([20], stddev=.1, dtype=tf.float32))
+        self.F3 = tf.Variable(tf.random.truncated_normal([3, 3, 20, 20], stddev=.1, dtype=tf.float32))
+        self.b3_F = tf.Variable(tf.random.truncated_normal([20], stddev=.1, dtype=tf.float32))
 
-        self.linear_b1 = tf.Variable(tf.random.truncated_normal([self.hidden_layer2], stddev=0.1))
-        self.linear_b2 = tf.Variable(tf.random.truncated_normal([self.num_classes], stddev=0.1))
-        self.linear_W1 = tf.Variable(tf.random.truncated_normal([self.hidden_layer1, self.hidden_layer2], stddev=0.1))
-        self.linear_W2 = tf.Variable(tf.random.truncated_normal([self.hidden_layer2, self.num_classes], stddev=0.1))
+        self.hidden_size = 80
+
+        self.W1 = tf.Variable(tf.random.normal([self.hidden_size, self.hidden_size], stddev=.1, dtype=tf.float32))
+        self.b1_W = tf.Variable(tf.random.normal([self.hidden_size], stddev=.1, dtype=tf.float32))
+        self.W2 = tf.Variable(tf.random.normal([self.hidden_size, self.hidden_size], stddev=.1, dtype=tf.float32))
+        self.b2_W = tf.Variable(tf.random.normal([self.hidden_size], stddev=.1, dtype=tf.float32))
+        self.W3 = tf.Variable(tf.random.normal([self.hidden_size, self.num_classes], stddev=.1, dtype=tf.float32))
+        self.b3_W = tf.Variable(tf.random.normal([self.num_classes], stddev=.1, dtype=tf.float32))
+
+        # self.batch_size = 100
+        # self.num_epoch = 1
+        # self.num_classes = 9    # ratings of 1.0, 1.5 ... 5.0 stars
+        # self.dropout_rate = 0.5
+        # self.learning_rate = 1e-3
+        # self.hidden_layer1 = 80
+        # self.hidden_layer2 = 40
+        # self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.learning_rate)
+
+        # self.conv1 = tf.Variable(tf.random.truncated_normal([5, 5, 3, 5], stddev=0.1))
+        # self.b1 = tf.Variable(tf.random.truncated_normal([5], stddev=0.1))
+
+        # self.conv2 = tf.Variable(tf.random.truncated_normal([5, 5, 5, 10], stddev=0.1))
+        # self.b2 = tf.Variable(tf.random.truncated_normal([10], stddev=0.1))
+
+        # self.linear_b1 = tf.Variable(tf.random.truncated_normal([self.hidden_layer2], stddev=0.1))
+        # self.linear_b2 = tf.Variable(tf.random.truncated_normal([self.num_classes], stddev=0.1))
+        # self.linear_W1 = tf.Variable(tf.random.truncated_normal([self.hidden_layer1, self.hidden_layer2], stddev=0.1))
+        # self.linear_W2 = tf.Variable(tf.random.truncated_normal([self.hidden_layer2, self.num_classes], stddev=0.1))
 
     def call(self, inputs):
-        # BASIC NET
-        inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
-        # CONVOLUTION LAYER 1
-        x = tf.nn.conv2d(inputs, self.conv1, strides=[1,1,1,1], padding='SAME')
-        x = tf.nn.bias_add(x, self.b1)
-        # MAX POOLING 1
-        x = tf.nn.max_pool(x, ksize=[2,2], strides=[1,1,1,1], padding='SAME')
-        # RELU 1
-        x = tf.nn.relu(x)
+        # # BASIC NET
+        # inputs = tf.convert_to_tensor(inputs, dtype=tf.float32)
+        # # CONVOLUTION LAYER 1
+        # x = tf.nn.conv2d(inputs, self.conv1, strides=[1,1,1,1], padding='SAME')
+        # x = tf.nn.bias_add(x, self.b1)
+        # # MAX POOLING 1
+        # x = tf.nn.max_pool(x, ksize=[2,2], strides=[1,1,1,1], padding='SAME')
+        # # RELU 1
+        # x = tf.nn.relu(x)
 
-        #CONVOLUTION LAYER 2
-        x = tf.nn.conv2d(x, self.conv2, strides=[1,1,1,1], padding='SAME')
-        x = tf.nn.bias_add(x, self.b2)
-        # MAX POOLING 2
-        x = tf.nn.max_pool(x, ksize=[2,2], strides=[1,1,1,1], padding='SAME')
-        # RELU 2
-        x = tf.nn.relu(x)
+        # #CONVOLUTION LAYER 2
+        # x = tf.nn.conv2d(x, self.conv2, strides=[1,1,1,1], padding='SAME')
+        # x = tf.nn.bias_add(x, self.b2)
+        # # MAX POOLING 2
+        # x = tf.nn.max_pool(x, ksize=[2,2], strides=[1,1,1,1], padding='SAME')
+        # # RELU 2
+        # x = tf.nn.relu(x)
 
-        # RESHAPE
-        x = tf.reshape(x, [-1, self.hidden_layer1])
+        # # RESHAPE
+        # x = tf.reshape(x, [-1, self.hidden_layer1])
 
-        # DENSE LAYER 1
-        x = tf.matmul(x, self.linear_W1) + self.linear_b1
-        x = tf.nn.relu(x)
-        x = tf.nn.dropout(x, rate=self.dropout_rate)
+        # # DENSE LAYER 1
+        # x = tf.matmul(x, self.linear_W1) + self.linear_b1
+        # x = tf.nn.relu(x)
+        # x = tf.nn.dropout(x, rate=self.dropout_rate)
 
-        # DENSE LAYER 2
-        x = tf.matmul(x, self.linear_W2) + self.linear_b2
+        # # DENSE LAYER 2
+        # x = tf.matmul(x, self.linear_W2) + self.linear_b2
+
+        conv_1 = tf.nn.conv2d(inputs, filters=self.F1, strides=[1, 2, 2, 1], padding="SAME")
+        conv_1 = tf.nn.bias_add(conv_1, self.b1_F)
+        mean, variance = tf.nn.moments(conv_1, axes=[0, 1, 2])
+        norm_1 = tf.nn.batch_normalization(conv_1, mean=mean, variance=variance, offset=None, scale=None, variance_epsilon=1e-5)
+        relu_1 = tf.nn.relu(norm_1)
+        max_pool_1 = tf.nn.max_pool(relu_1, ksize=3, strides=2, padding="SAME")
+
+        conv_2 = tf.nn.conv2d(max_pool_1, filters=self.F2, strides=[1, 2, 2, 1], padding="SAME")
+        conv_2 = tf.nn.bias_add(conv_2, self.b2_F)
+        mean, variance = tf.nn.moments(conv_2, axes=[0, 1, 2])
+        norm_2 = tf.nn.batch_normalization(conv_2, mean=mean, variance=variance, offset=None, scale=None, variance_epsilon=1e-5)
+        relu_2 = tf.nn.relu(norm_2)
+        max_pool_2 = tf.nn.max_pool(relu_2, ksize=2, strides=2, padding="SAME")
+
+        conv_3 = tf.nn.conv2d(max_pool_2, filters=self.F3, strides=[1, 1, 1, 1], padding="SAME")
+        conv_3 = tf.nn.bias_add(conv_3, self.b3_F)
+        mean, variance = tf.nn.moments(conv_2, axes=[0, 1, 2])
+        norm_3 = tf.nn.batch_normalization(conv_3, mean=mean, variance=variance, offset=None, scale=None, variance_epsilon=1e-5)
+        relu_3 = tf.nn.relu(norm_3)
+        relu_3 = tf.reshape(relu_3, (len(inputs), -1))  # reshape before linear layers
+
+        dense_1 = tf.nn.relu(tf.nn.dropout(tf.matmul(relu_3, self.W1) + self.b1_W, rate=0.3))
+        dense_2 = tf.nn.relu(tf.nn.dropout(tf.matmul(dense_1, self.W2) + self.b2_W, rate=0.3))
+        dense_3 = tf.matmul(dense_2, self.W3) + self.b3_W
+
+        return dense_3
 
 
         # RESNET
@@ -70,10 +120,12 @@ class YelpClassifier(tf.keras.Model):
 
         # x = self.linear(x)
 
-        return x
+        # return x
 
     def loss(self, logits, labels):
-        softmax_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits, axis=-1))
+        # softmax_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits, axis=-1))
+        softmax_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits))
+        # softmax_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits))
         return softmax_loss
 
     def accuracy(self, logits, labels):
@@ -92,8 +144,9 @@ def train(model, train_inputs, train_labels):
 
         with tf.GradientTape() as tape:
             logits = model.call(inputs_batch)
-            print("LOOK HERE")
-            print(tf.shape(labels_batch))
+            # print("LOOK HERE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            # print(tf.shape(inputs_batch))
+            # print(tf.shape(labels_batch))
             loss = model.loss(logits, labels_batch)
 
         losses.append(loss)
@@ -119,7 +172,7 @@ def test(model, test_inputs, test_labels):
 
 def main():
     # Return the training and testing data and labels from get_data
-    train_data, train_labels, test_data, test_labels = get_data("../yelp-data/yelp_academic_dataset_business.json", "../yelp-data/photos.json", "../yelp-data/photos", "food")
+    train_data, train_labels, test_data, test_labels = get_data("data/json/menu.json", "../yelp-data/photos", size=[32, 32])
 
     # Instantiate model
     m = tf.keras.applications.ResNet101V2()
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,10 @@
 import json
 from matplotlib.image import imread
-from cv2 import imread, resize
+# from cv2 import resize
 import tensorflow as tf
 import numpy as np
+from progressbar import ProgressBar
+pbar = ProgressBar()
 
 def read_from_json_file(filepath):
     """
@@ -10,14 +12,12 @@ def read_from_json_file(filepath):
     :param filepath: path to JSON file
     :return: a list of dictionaries (one dict for each JSON object)
     """
-    data = []
     with open(filepath) as infile:
-        for l in infile:
-            data.append(json.loads(l))
+        data = json.load(infile)
     print("Read in", len(data), "objects from", filepath)
     return data
 
-def get_data(business_json_filepath, image_json_filepath, image_filepath, image_label, size=[32, 32], test_fraction=0.2):
+def get_data(image_json_filepath, image_filepath, size=[32, 32], test_fraction=0.2):
     """
     Reads business and image JSON files and images to return data for training and testing.
     :param business_json_filepath: path to yelp_academic_dataset_business.json
@@ -28,40 +28,28 @@ def get_data(business_json_filepath, image_json_filepath, image_filepath, image_
     :param test_fraction: fraction of data for testing
     :return: training and testing data and labels for given label from business and image JSON
     """
-    assert(image_label in ['food', 'menu', 'drink', 'inside', 'outside'])
-    # Read in business JSON
-    businesses = read_from_json_file(business_json_filepath)
-
-    # Create dictionary of business_id -> stars
-    business_ratings = {}
-    for b in businesses:
-        business_ratings[b['business_id']] = b['stars']
+    print("Preprocessing.")
 
     # Read in image JSON
     images = read_from_json_file(image_json_filepath)
 
-    # TO DELETE
-    images = images[:1000] # first 1000 for testing purposes
-
     # Categorize images by label
     data = []
     labels = []
-    for i in images:
-        # if i['label'] == label and i['business_stars'] % 1 == 0:
-        if i['label'] == image_label:
+    print("Reading in images.")
+    for i in pbar(images):
+        if i['business_stars'] % 1 == 0:
             # Add business_stars to labels
-            labels.append(business_ratings[i['business_id']])
+            labels.append(int(i['business_stars']) - 1)  # label 0 = star 1
             # Read image as numpy array
-            data.append(imread(image_filepath + '/' + i['photo_id'] + '.jpg'))
+            img = tf.convert_to_tensor(imread(image_filepath + '/' + i['photo_id'] + '.jpg'), dtype=tf.float32)
+            img = tf.image.resize(img, size)
+            data.append(img)
 
-    # Resize images to given size
-    # data = tf.image.resize(tf.convert_to_tensor(data), size)
-    resized_data = []
-    for d in data:
-        resized_data.append(resize(d, tuple(size)))
-    data = resized_data
+    data = tf.convert_to_tensor(data)
+    labels = tf.one_hot(labels, 5)
 
-    print("Found", len(data), "images with label", labels)
+    print("Collected", len(data), "images based on criteria")
 
     # Split for training and testing
     split_index = int(len(data) * (1 - test_fraction))
