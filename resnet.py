e54e06f997971b027993014771ab27fc93bb9ae7
brandonqlee <brandonqlee@hotmail.com>
2020-12-01 20:43:50 -0500

    drink

--- resnet.py
+++ resnet.py
@@ -35,13 +35,13 @@ model = Model(inputs=base_model.input, outputs=predictions)
 #     layer.trainable = False
  
 # Compiling the model
-model.compile(optimizer=SGD(lr=0.00001, momentum=0.9), loss='categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics = ['accuracy'])
 
 # Return the training and testing data and labels from get_data
 train_data, train_labels, test_data, test_labels = get_data("data/json/outside.json", "../yelp-data/photos", size=[75, 75], test_one_hot=False)
 
 # Training the model!
-model.fit(train_data, train_labels, batch_size=100, epochs=80, verbose=1)
+model.fit(train_data, train_labels, batch_size=100, epochs=40, verbose=1)
  
 # Saving the weights and model architecture
 save_model(model, "resnet_weights.h5", "resnet_model.json")
