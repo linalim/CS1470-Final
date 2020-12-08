130a5d8f38cfa04802e74350997fd890eca8c0bf
Seo Hyun (Lina) Lim <seohyunlim98@gmail.com>
2020-12-09 01:10:35 +0900

    specifying dtype float for plt imshow

--- gan.py
+++ gan.py
@@ -156,7 +156,7 @@ def main():
     noise = tf.random.normal([1, 100])
     generated_image = generator(noise, training=False)
 
-    plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+    plt.imshow(np.array(generated_image[0, :, :, 0], dtype=float))
 
     # Instantiate discriminator model
     discriminator = gan.make_discriminator_model()
